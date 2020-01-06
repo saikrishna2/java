5eb6891483767061e3f4842ae1c76f8d1478a856
krishna <w.saikrishnakumarraju1234@gmail.com>
2020-01-06 01:15:39 -0800

    HI

---
+++ a.java
@@ -0,0 +1,24 @@
+import java.io.*;
+public class Krishna
+{
+     public static void main(String []args){
+        {
+           for (i = 2; i <= n / 2; ++i) {
+        
+        if (n % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+  
+        if (flag == 0)
+            printf("%d is a prime, n);
+        else
+            printf("%d is not ", n);
+
+        }
+        
+        }
+        
+     
+}
